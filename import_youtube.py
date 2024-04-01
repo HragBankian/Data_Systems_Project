78d1739e3987810d8009db2d34f45d3695d514d3
NicholasWahome <87381233+NicholasWahome@users.noreply.github.com>
2024-04-01 01:07:02 -0400

    Removed some unecessary code until ddl is done

--- import_youtube.py
+++ import_youtube.py
@@ -3,13 +3,6 @@ import pyodbc
 from ytmusicapi import YTMusic
 import os
 ytmusic = YTMusic("oauth.json")
-def connect_to_sql_server():
-    conn = pyodbc.connect('DRIVER={SQL Server};'
-                          'SERVER=' + os.getenv('DB_SERVER') + ';'
-                          'DATABASE=' + os.getenv('DB_DATABASE') + ';'
-                          'UID=' + os.getenv('DB_UID') + ';'
-                          'PWD=' + os.getenv('DB_PASSWORD'))
-    return conn.cursor()
 
 # Function to insert data into SQL table
 def insert_into_sql_table(cursor, title, artist):
@@ -32,15 +25,4 @@ ytmusic = YTMusic("oauth.json")
 
 # Perform search
 search_results = ytmusic.search("Oasis Wonderwall", filter="songs")
-
-# Connect to SQL Server
-cursor = connect_to_sql_server()
-
-# Parse JSON data and insert into SQL table
-for result in search_results:
-    title = result['title']
-    artist = result['artists'][0]['name']
-    insert_into_sql_table(cursor, title, artist)
-
-# Close the cursor and connection
-cursor.close()
+print(search_results)
